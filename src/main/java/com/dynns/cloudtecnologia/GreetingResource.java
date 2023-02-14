e2dd6bfcb078a87ef04151f11a7bed731e017a64
Thiago Junior Amorim De Melo (GOVBR GO - DRC Central) <thiago.melo@govbr.com.br>
2023-02-14 13:48:36 -0300

    Envio de email

--- src/main/java/com/dynns/cloudtecnologia/GreetingResource.java
+++ src/main/java/com/dynns/cloudtecnologia/GreetingResource.java
@@ -1,16 +1,46 @@
 package com.dynns.cloudtecnologia;
 
+
+import io.quarkus.mailer.reactive.ReactiveMailer;
+import io.quarkus.mailer.*;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Path("/hello")
+@Produces(MediaType.APPLICATION_JSON)
 public class GreetingResource {
 
+    @Inject
+    Mailer mailer;
+
+    @Inject
+    ReactiveMailer reactiveMailer;
+
+    private static final String DESTINATARIO = "thi4go19@gmails.com";
+
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from RESTEasy Reactive";
+    @Path("/mail-assincrona")//envia e não aguarda retorno
+    public Response assincrona() {
+        List<String> destinatarios = new ArrayList<>();
+        destinatarios.add("john.doe@example.com");
+        //
+        Mail mail = new Mail();
+        mail.setTo(destinatarios);
+        mail.setSubject("Assunto aqui");
+        mail.setText("Hello from Quarkus!");
+        //
+        mailer.send(mail);
+        return Response.accepted().build();
     }
+
+
 }
